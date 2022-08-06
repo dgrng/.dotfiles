cfd7cecec630c000f3a01b83121b44fe58bc216b
Mandip Gurung <maandiptammu@gmail.com>
2022-08-06 21:54:50 +0900

    npm config

--- setup.sh
+++ setup.sh
@@ -11,6 +11,7 @@ export ZDOTDIR="$XDG_CONFIG_HOME/zsh"
 mkdir -p "$ZDOTDIR"
 
 ln -sf "$DOTFILES/zsh/zshenv" "$HOME/.zshenv"
+ln -sf "$DOTFILES/zsh/zprofile" "$ZDOTDIR/.zprofile"
 ln -sf "$DOTFILES/zsh/zshrc" "$ZDOTDIR/.zshrc"
 ln -sf "$DOTFILES/zsh/extra" "$ZDOTDIR/extra"
 
