cf9a3cbbc41f2dfaece57a6ea9f52783daef2933
Mandip Gurung <gurung@gurung.local>
2022-08-06 21:05:48 +0900

    add setup and zsh

---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+export DOTFILES="$HOME/.dotfiles"
+# xdg config
+export XDG_CONFIG_HOME="$HOME/.config"
+
+mkdir -p "$XDG_CONFIG_HOME"
+
+## zsh
+export ZDOTDIR="$XDG_CONFIG_HOME/zsh"
+mkdir -p "$ZDOTDIR"
+
+ln -sf "$DOTFILES/zsh/zshenv" "$HOME/.zshenv"
+ln -sf "$DOTFILES/zsh/zshrc" "$ZDOTDIR/.zshrc"
+ln -sf "$DOTFILES/zsh/extra" "$ZDOTDIR/extra"
+
+## neovim
+ln -sf "$DOTFILES/nvim" "$XDG_CONFIG_HOME/nvim"
+
+## alacritty
+ln -sf $DOTFILES/alacritty $XDG_CONFIG_HOME/alacritty
+
+## tmux
+ln -sf "$DOTFILES/tmux" "$XDG_CONFIG_HOME/tmux"
+
+## i3
+ln -sf "$DOTFILES/i3" "$XDG_CONFIG_HOME/i3"
+ln -sf "$DOTFILES/i3status" "$XDG_CONFIG_HOME/i3status"
