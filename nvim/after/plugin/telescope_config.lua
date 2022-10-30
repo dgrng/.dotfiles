07eb37624314996811fa75df70be08a5834960d5
Mandip Gurung <8016611+dgrng@users.noreply.github.com>
2022-10-30 13:58:27 +0900

    telescope config

--- nvim/after/plugin/telescope_config.lua
+++ nvim/after/plugin/telescope_config.lua
@@ -1,9 +1,11 @@
 local telescope = require('telescope')
 local builtin = require('telescope.builtin')
 
-telescope.setup{
+telescope.setup {
    defaults = {
-      file_ignore_patterns = {"node_modules", ".git", ".vscode", ".next"}
+      prompt_prefix = " >",
+      color_devicons = true,
+      file_ignore_patterns = { "node_modules", ".git", ".vscode", ".next" }
    },
    pickers = {
       find_files = {
@@ -20,25 +22,25 @@ telescope.setup{
 telescope.load_extension("file_browser")
 
 vim.keymap.set("n",
-"<leader>fn",
-function()
-telescope.extensions.file_browser.file_browser({
-   hidden = "true"
-})
+   "<leader>fn",
+   function()
+      telescope.extensions.file_browser.file_browser({
+         hidden = "true"
+      })
+   end)
+
+vim.keymap.set("n", "<leader>ff", function()
+   builtin.find_files()
 end)
 
-vim.keymap.set("n", "<leader>ff",function()
-builtin.find_files()
+vim.keymap.set("n", "<leader>fg", function()
+   builtin.live_grep()
 end)
 
-vim.keymap.set("n", "<leader>fg",function()
-builtin.live_grep()
+vim.keymap.set("n", "<leader>fb", function()
+   builtin.buffers()
 end)
 
-vim.keymap.set("n", "<leader>fb",function()
-builtin.buffers()
-end)
-
-vim.keymap.set("n", "<leader>fh",function()
-builtin.help_tags()
+vim.keymap.set("n", "<leader>fh", function()
+   builtin.help_tags()
 end)
