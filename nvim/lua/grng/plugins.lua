cfea37004d875c166e8a4b8d02d46634fe3196d1
GURUNG MANDIP <8016611+dgrng@users.noreply.github.com>
2024-01-19 17:11:40 +0900

    telescope upgrade

--- nvim/lua/grng/plugins.lua
+++ nvim/lua/grng/plugins.lua
@@ -8,7 +8,7 @@ return require('packer').startup(function(use)
     use({ 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' })
 
     use {
-        'nvim-telescope/telescope.nvim',
+        'nvim-telescope/telescope.nvim', tag = '0.1.4',
         requires = { { 'nvim-lua/plenary.nvim' } }
     }
 
